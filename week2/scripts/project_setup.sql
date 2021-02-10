98b6679beb7b21358b74f25b6aad63dc67e65928
IkenoXamos <moberlies1337@gmail.com>
2021-02-10 18:18:54 -0500

    Add project_setup sql script

---
+++ week2/scripts/project_setup.sql
@@ -0,0 +1,26 @@
+DROP TYPE IF EXISTS project0.role CASCADE;
+CREATE TYPE project0.role AS ENUM ('Admin', 'Employee', 'Customer');
+
+DROP TABLE IF EXISTS project0.users CASCADE;
+CREATE TABLE project0.users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR (250) UNIQUE NOT NULL,
+	password VARCHAR (250) NOT NULL,
+	role project0.ROLE NOT NULL
+);
+
+DROP TABLE IF EXISTS project0.accounts CASCADE;
+CREATE TABLE project0.accounts (
+	id SERIAL PRIMARY KEY,
+	balance NUMERIC (50, 2) NOT NULL CHECK (balance >= 0) DEFAULT 0,
+	owner INTEGER NOT NULL REFERENCES project0.users (id)
+--	active BOOLEAN DEFAULT false
+);
+
+DROP TABLE IF EXISTS project0.applications CASCADE;
+CREATE TABLE project0.applications (
+	id SERIAL PRIMARY KEY,
+	owner INTEGER NOT NULL REFERENCES project0.users (id)
+);
+
+INSERT INTO users (username, password, role) VALUES ('moberlies', 'password', 'Admin');
